3d51d1c39589e8a48cca9bd83ae606d8d7cd20a2
AtiqBytes <atiqbytes@gmail.com>
2024-08-15 14:52:49 +0500

    created a grocery reducer and added intialstate of the grocery with 3 items

---
+++ src/app/store/reducers/grocery.reducer.ts
@@ -0,0 +1,10 @@
+import { Grocery } from "../../../models/grocery.model";
+
+const initialState:Grocery[]=[
+    {"id":1, "name":"milk",type:"fruit"},
+    {"id":2, "name":"Banana", type : "fruit"},
+    {"id":3, "name" : "Smily chips", type : "snacks"},
+    {"id":4, "name" : "Salman Choran", type : "snacks" }
+]
+
+

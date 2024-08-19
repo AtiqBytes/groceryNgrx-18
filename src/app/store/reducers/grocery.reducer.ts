7507190a59ff9b6de5ce459b8b5b1b4701e9eb17
AtiqBytes <atiqbytes@gmail.com>
2024-08-19 15:54:12 +0500

    making initial state of grocery empty in 'grocery.reducer.ts' because we are going ot get the initial state from the mock Api 'apiServer'

--- src/app/store/reducers/grocery.reducer.ts
+++ src/app/store/reducers/grocery.reducer.ts
@@ -1,12 +1,14 @@
 import { createReducer } from "@ngrx/store";
 import { Grocery } from "../../../models/grocery.model";
 
-const initialState:Grocery[]=[
-    {"id":1, "name":"milk",type:"fruit"},
-    {"id":2, "name":"Banana", type : "fruit"},
-    {"id":3, "name" : "Smily chips", type : "snacks"},
-    {"id":4, "name" : "Salman Choran", type : "snacks" }
-]
+// const initialState:Grocery[]=[
+//     {"id":1, "name":"milk",type:"fruit"},
+//     {"id":2, "name":"Banana", type : "fruit"},
+//     {"id":3, "name" : "Smily chips", type : "snacks"},
+//     {"id":4, "name" : "Salman Choran", type : "snacks" }
+// ]
+
+const initialState:Grocery[]=[]
 
 
 export const groceryReducer = createReducer(initialState);

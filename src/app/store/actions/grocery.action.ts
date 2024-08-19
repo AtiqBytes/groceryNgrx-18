19c62ca234dc76c5647b8bff0e735c342e0358ba
AtiqBytes <atiqbytes@gmail.com>
2024-08-19 15:28:20 +0500

    created Action Group to group all possible related actions And Use It From One Place

--- src/app/store/actions/grocery.action.ts
+++ src/app/store/actions/grocery.action.ts
@@ -1,5 +1,23 @@
-import { createAction } from "@ngrx/store";
+// import { createAction } from "@ngrx/store";
 
-export const initGroceries = createAction('[Grocery] Load Groce')
+// export const initGroceries = createAction('[Grocery] Load Groce')
 
-export const  completedGroceries = createAction('[Grocery] Load Groceries Success' )
+// export const  completedGroceries = createAction('[Grocery] Load Groceries Success' )
+
+
+import { createActionGroup, emptyProps, props } from '@ngrx/store';
+import { Grocery } from '../../../models/grocery.model';
+
+export const groceryAction = createActionGroup({
+  source: 'Grocery API',
+  events: {
+    // defining an event without payload using the `emptyProps` function
+    'Load Groceries': emptyProps(),
+
+    // defining an event with payload using the `props` function
+    'Load Groceries Success': props<{ payload:Grocery[] }>(),
+
+    // defining an event with payload using the props factory
+    'Load Groceries Failure': emptyProps,
+  },
+});

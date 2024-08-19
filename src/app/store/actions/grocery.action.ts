a25a418d43ed7dd0d791d59ec9d9e14defaa7e9f
AtiqBytes <atiqbytes@gmail.com>
2024-08-19 15:13:58 +0500

    1- created 'grocery effects' to fetch data from external mock api.
    2- created Grocery Actions To fetch groceries from mock api

---
+++ src/app/store/actions/grocery.action.ts
@@ -0,0 +1,5 @@
+import { createAction } from "@ngrx/store";
+
+export const initGroceries = createAction('[Grocery] Load Groce')
+
+export const  completedGroceries = createAction('[Grocery] Load Groceries Success' )
